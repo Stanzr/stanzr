7b2decce621ccea60c6a3e7e05090600d613f212
Richard Rodger <richard@ricebridge.com>
2011-04-13 17:13:37 +0100

    chat

--- site/public/js/stanzr.js
+++ site/public/js/stanzr.js
@@ -2,16 +2,48 @@
 
 $(document).ready(function(){
       
-  now.name = prompt("What's your name?", "");  
-  now.receiveMessage = function(name, message){
-        $("#messages").append("<br>" + name + ": " + message);
+  function enterkey(cb) {
+    return function(event) {
+      if( 13 == event.keyCode ) {
+        cb()
+      }
+    }
   }
+
+  function login(){
+    var name = $('#username').val()
+
+    if( 0 < name.length ) {
+      $('#logindiv').fadeOut(function(){
+        $('div.topicwelcome h2 span').text(name)
+        $('div.topicwelcome').fadeIn()
+        $('div.topicsend').fadeIn(function(){
+          $("#post_text").focus()
+        })
+      })
+
+      now.name = name
+
+      now.receiveMessage = function(name, message){
+        var post = $('#posts_tm li').clone()
+        post.find('h4').text(name)
+        post.find('p').text(message)
+        $('#posts').prepend(post)
+      }
   
-  $("#send-button").click(function(){
-    now.distributeMessage($("#text-input").val());
-    $("#text-input").val("");
-    $("#text-input").focus();
-  });
+      function post(){
+        now.distributeMessage($("#post_text").val());
+        $("#post_text").val("");
+        $("#post_text").focus();
+      }
+
+      $("#post_send").click(post)
+      $("#post_text").keypress(enterkey(post))
+    }
+  }
+
+  $('#loginbtn').click(login)
+  $('#username').focus().keypress(enterkey(login))
       
 });
 
