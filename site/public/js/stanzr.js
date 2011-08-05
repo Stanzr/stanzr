27f0d1f7d07f8d54e8ea0038e48b0e3992540cd9
Daniel Lewis <daniel@cloudspace.com>
2011-08-04 20:24:41 -0400

    [15292331] most-liked messages are per-topic in view.

--- site/public/js/stanzr.js
+++ site/public/js/stanzr.js
@@ -197,7 +197,10 @@ var app = {
 
     $('ul.topicposts').hide()
     app.topicposts = $('#topic_posts_'+app.topic).show()
-
+    
+    $('#rally_agree_container ul').hide()
+    $('#rally_agree_' + topic).show()
+    
     app.resize()
 
     app.scrolldown()
@@ -1886,7 +1889,6 @@ function DirectMessageBox() {
 DirectMessageBox.prototype = new RightbarBox()
 
 
-
 function AgreeBox() {
   var self = this
 
@@ -1900,7 +1902,9 @@ function AgreeBox() {
     ,drillup: $('#agree_drillup')
     ,box: $('#agree_box')
 
-    ,msgs: $('#rally_agree')
+    ,msgs_tm: $('#rally_agree_tm')
+    ,msg_lists_container: $('#rally_agree_container')
+    ,msg_lists: {}
     ,msg_tm: $('#agree_msg_tm')
 
   }
@@ -1935,7 +1939,9 @@ function AgreeBox() {
     })
     debug('post-sort',agrees)
 
-    self.el.msgs.empty()
+    for (var i in self.el.msg_lists) {
+        self.el.msg_lists[i].empty()
+    }
 
     self.count = 0
     for( var i = 0; i < agrees.length; i++ ) {
@@ -1945,14 +1951,22 @@ function AgreeBox() {
           msgdiv.find('h4').text(msg.f)
           msgdiv.find('.count').text('x'+msg.a)
           msgdiv.find('.post').text(msg.t)
-
           if( !msg.h ) {
-            self.el.msgs.append(msgdiv)
+            // if we don't have a div for this topic (msg.p) yet, create one
+            if( ! (msg.p in self.el.msg_lists) ) {
+                var new_msg_list = self.el.msgs_tm.clone().attr('id', 'rally_agree_'+msg.p)
+                if ( $.isEmptyObject(self.el.msg_lists) ) {
+                    new_msg_list.css('display','block')
+                }
+                self.el.msg_lists[msg.p] = new_msg_list
+                self.el.msg_lists_container.append(new_msg_list)
+            }
+            self.el.msg_lists[msg.p].append(msgdiv)
             msgdiv.fadeIn()
             self.count++
           }
-
-          if( 'up' == self.drill && 100 < self.el.msgs.height() ) {
+          
+          if( 'up' == self.drill && 100 < self.el.msg_lists[msg.p].height() ) {
             i = agrees.length
           }
         }
